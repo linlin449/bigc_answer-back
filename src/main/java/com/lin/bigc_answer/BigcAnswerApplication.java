66241ab6962e3d7556b2336bba3a0342110e3a37
lin <1791598990@qq.com>
2022-07-06 17:38:24 +0800

    sql数据库文件上传

--- src/main/java/com/lin/bigc_answer/BigcAnswerApplication.java
+++ src/main/java/com/lin/bigc_answer/BigcAnswerApplication.java
@@ -1,11 +1,12 @@
 package com.lin.bigc_answer;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-//@ServletComponentScan
 public class BigcAnswerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BigcAnswerApplication.class, args);
